c718bb8d1bfc4b9d1c126b65cb09ab89e81b77b8
Rishabh <rishabh8n@gmail.com>
2024-01-17 23:17:36 +0530

    fix: try catch for auth middleware

--- server/src/middlewares/auth.middleware.ts
+++ server/src/middlewares/auth.middleware.ts
@@ -11,18 +11,22 @@ export interface CustomRequest extends Request {
   user?: UserProps;
 }
 export const verifyJWT = asyncHandler(async (req: CustomRequest, res, next) => {
-  const token =
-    req.cookies?.ACCESS_TOKEN ||
-    req.header("Authorization")?.replace("Bearer ", "");
-  if (!token) throw new ApiError(401, "Unauthorized request");
-  let sec = process.env.ACCESS_TOKEN_SECRET as string;
-  const decoded = jwt.verify(token, sec) as DecodedData;
-  const user = await User.findById(decoded?._id).select(
-    "-password -refreshToken",
-  );
-  if (!user) {
-    throw new ApiError(401, "Invalid Access Token");
+  try {
+    const token =
+      req.cookies?.ACCESS_TOKEN ||
+      req.header("Authorization")?.replace("Bearer ", "");
+    if (!token) throw new ApiError(401, "Unauthorized request");
+    let sec = process.env.ACCESS_TOKEN_SECRET as string;
+    const decoded = jwt.verify(token, sec) as DecodedData;
+    const user = await User.findById(decoded?._id).select(
+      "-password -refreshToken",
+    );
+    if (!user) {
+      throw new ApiError(401, "Invalid Access Token");
+    }
+    req.user = user;
+    next();
+  } catch (error: any) {
+    new ApiError(401, error.message || "unable to process request");
   }
-  req.user = user;
-  next();
 });
